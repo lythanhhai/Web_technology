6405f1cafbae9ff10e160c3085faf49fd4f354b4
lythanhhai <you@example.com>
2021-10-24 21:10:56 +0700

    phan quyen cho khach hoac admin

---
+++ DynamicWeb/BT1/T9.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '
+            <ul class="list" type="none">
+                <li><h4>Trang chủ</h4></li>
+                <li><a href="./xemTTNhanVien.php" target="T3">Xem nhân viên</a></li>
+                <br>
+                <li ><a href="./xemTTPhongBan.php" target="T3">Xem thông tin phòng ban</a></li>
+                <br>
+                <li><a href="./timKiemNV.php" target="T3">Tìm kiếm</a></li>
+                <br>
+                <li ><a href="./timkiem.php" target="T3">tìm kiếm theo thuộc tính</a></li>
+            </ul>
+            ';
+    ?>
+</body>
+</html>
--- DynamicWeb/BT1/xuLyLogin.php
+++ DynamicWeb/BT1/xuLyLogin.php
@@ -10,25 +10,38 @@
     <?php
         $username = $_REQUEST['username'];
         $password = $_REQUEST['password'];
-        if($username == "" || $password == "") { header("Location:login.php");}
+        $guest = "guest";
+        // nếu là khách
+        if(isset($_POST["guest"]))
+        {
+            $guest = "guest";
+            header("Location: index1.php?guest=".$guest);
+        }
         else 
         {
-            // kết nối với cơ sở dữ liệu
-            $link = mysqli_connect("localhost:3307","root","") or die ("khong the ket noi");
-            // chọn loại kết nối
-            mysqli_select_db($link,"dulieu1");
-            $sql ="select * from admin where Username='$username' and Password='$password'";
-            
-            // lấy kết quả từ query
-            $result= mysqli_query($link, $sql);
-            if(mysqli_num_rows($result) == 0) { header("Location:login.php");}
+            //if($guest !== "")
+            if($username == "" || $password == "") { header("Location:login.php");}
             else 
             {
-                header("Location:capNhat.php");
+                // kết nối với cơ sở dữ liệu
+                $link = mysqli_connect("localhost:3307","root","") or die ("khong the ket noi");
+                // chọn loại kết nối
+                mysqli_select_db($link,"dulieu1");
+                $sql ="select * from admin where Username='$username' and Password='$password'";
+                
+                // lấy kết quả từ query
+                $result= mysqli_query($link, $sql);
+                if(mysqli_num_rows($result) == 0) { header("Location:login.php");}
+                else 
+                {
+                    $guest = "admin";
+                    header("Location: index1.php?guest=".$guest);
+                }
+                
+                mysqli_free_result($result);
+                mysqli_close($link);
             }
-            
-            mysqli_free_result($result);
-            mysqli_close($link);
+
         }
     ?>
         
